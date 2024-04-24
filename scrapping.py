3553de0e566afe6916d84d82c2f740c5fcefd530
bartoszduch <139812577+bartoszduch@users.noreply.github.com>
2024-04-24 16:24:15 +0200

    version0

--- scrapping.py
+++ scrapping.py
@@ -24,7 +24,7 @@ def download_images_from_page(url, save_directory, max_images=20):
 
 
 # Adres URL strony zawierającej obrazy
-page_url = 'https://pl.freepik.com/darmowe-zdjecie-wektory/owczarek-niemiecki'
+page_url = 'https://www.pexels.com/pl-pl/szukaj/owczarek%20niemiecki/'
 
 # Katalog docelowy, gdzie zostaną zapisane obrazy
 save_directory = 'new_data_directory'
